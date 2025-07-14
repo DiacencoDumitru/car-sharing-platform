5faa2ef2c6fc5733ab4b3b6ce90337ccaaad1e1c
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-07-14 09:15:24 +0300

    Setting Up implementation

--- src/main/java/com/dynamiccarsharing/carsharing/model/ContactInfo.java
+++ src/main/java/com/dynamiccarsharing/carsharing/model/ContactInfo.java
@@ -1,34 +1,49 @@
 package com.dynamiccarsharing.carsharing.model;
 
-import com.dynamiccarsharing.carsharing.util.Validator;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
-import lombok.With;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+import java.util.UUID;
 
 @Getter
 @ToString
-@EqualsAndHashCode
+@EqualsAndHashCode(exclude = "user")
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Entity
+@Table(name = "contact_infos")
 public class ContactInfo {
-    private final Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private final UUID id;
+
     @With
+    @NotBlank(message = "First name must be not null.")
+    @Column(name = "first_name", nullable = false)
     private final String firstName;
+
     @With
+    @NotBlank(message = "Last name must be not null.")
+    @Column(name = "last_name", nullable = false)
     private final String lastName;
+
     @With
+    @NotBlank(message = "Email is required and cannot be blank.")
+    @Email(message = "Please provide a valid email address.")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format.")
+    @Column(nullable = false, unique = true)
     private final String email;
+
     @With
+    @NotBlank(message = "Phone number must be not null.")
+    @Column(name = "phone_number", nullable = false)
     private final String phoneNumber;
 
-    public ContactInfo(Long id, String firstName, String lastName, String email, String phoneNumber) {
-        Validator.validateId(id, "ID");
-        Validator.validateNonEmptyString(firstName, "First name");
-        Validator.validateNonEmptyString(lastName, "Last name");
-        Validator.validateNonEmptyString(phoneNumber, "Phone number");
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
+    @OneToOne(mappedBy = "contactInfo")
+    private final User user;
 }
