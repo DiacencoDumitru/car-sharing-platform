c1c6c88c3e4fd532fd9cc7e612f377a44ce76fe1
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-08-31 16:34:08 +0300

    traces implementation

--- api-gateway/src/main/java/com/dynamiccarsharing/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/dynamiccarsharing/gateway/config/SecurityConfig.java
@@ -14,8 +14,8 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(ex -> ex
-                        .pathMatchers("/actuator/**").permitAll()
-                        .anyExchange().authenticated()
+                        .pathMatchers("/actuator/**", "/api/v1/**").permitAll()
+                        .anyExchange().permitAll()
                 )
                 .build();
     }
