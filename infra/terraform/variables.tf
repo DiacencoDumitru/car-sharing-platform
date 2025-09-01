b87fafbd28830ec25213c6aecf6b2113393287cc
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-09-01 10:43:02 +0300

    ci/cd new implementation

--- infra/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" { type = string }
-variable "ecr_registry" { type = string }
-variable "image_tag"   { type = string }
-
-variable "db_host"     { type = string, default = "your-rds-endpoint.rds.amazonaws.com" }
-variable "db_user"     { type = string, default = "postgres" }
-variable "db_password" { type = string, default = "password123" }
