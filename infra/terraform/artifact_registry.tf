d86fd311ba617fc195ba84d6dcd5b833406f5370
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-09-01 13:19:47 +0300

    deleted ci/cd for later

--- infra/terraform/artifact_registry.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "docker_repo" {
-  location      = var.region
-  repository_id = var.repo
-  description   = "Docker images for microservices"
-  format        = "DOCKER"
-}
