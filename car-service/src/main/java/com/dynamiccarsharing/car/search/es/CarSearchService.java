f686b42b7e147063f7136ab0238ac9e0ce17e26c
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-09-08 09:34:49 +0300

    no sql implementation with elastic search

---
+++ car-service/src/main/java/com/dynamiccarsharing/car/search/es/CarSearchService.java
@@ -0,0 +1,16 @@
+package com.dynamiccarsharing.car.search.es;
+
+import com.dynamiccarsharing.car.criteria.CarSearchCriteria;
+import com.dynamiccarsharing.contracts.dto.CarDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CarSearchService {
+
+    Page<CarDto> search(String q, CarSearchCriteria criteria, Pageable pageable);
+
+    void indexCar(Long carId);
+
+    void deleteFromIndex(Long carId);
+
+}
