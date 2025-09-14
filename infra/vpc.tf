fc3762084e4dc69423d29b0061cebfda64f72343
Dumitru Diacenco <ddiacenco@griddynamics.com>
2025-09-14 14:29:05 +0300

    CI/CD testing

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "${var.project_name}-vpc"
-  cidr = "10.0.0.0/16"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+  name    = "${var.project_name}-vpc"
+  cidr    = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
