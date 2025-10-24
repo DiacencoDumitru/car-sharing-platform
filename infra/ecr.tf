2d108266205aa0b38490f1ff5d17e96e0bd88e90
ddiacenco <ddiacenco@griddynamics.com>
2025-10-24 21:07:35 +0300

    gitignore updated, untrack big zise files

--- infra/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "car_service" {
-  name                 = "${var.project_name}/car-service"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
